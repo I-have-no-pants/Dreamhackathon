fb8af50e38e285b198f43071bef1b4b4fda9c671
woozie164 <woozie663@gmail.com>
2015-06-13 20:07:17 +0200

    Created a missile

---
+++ Assets/MissileLogic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileLogic : MonoBehaviour {
+	public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += velocity * Time.deltaTime;
+	}
+}
