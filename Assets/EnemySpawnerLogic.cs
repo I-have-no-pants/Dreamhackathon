449ac6550a441e19b4c7287a0bcde5b21e531f83
woozie164 <woozie663@gmail.com>
2015-06-13 20:38:48 +0200

    Added some logic to spawn enemies
    Not very happy with setting the enemy stats in code though. I'd rather
    do it in the editor.

---
+++ Assets/EnemySpawnerLogic.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerLogic : MonoBehaviour {
+	enum Direction {
+		DownRight,
+		DownLeft,
+		Left,
+		Right
+	}
+	enum SpawnPosition {
+		TopLeft,
+		TopMiddle,
+		TopRight,
+		MiddleLeft,
+		MiddleRight,
+	}
+	public Transform prefab;
+	// Use this for initialization
+	void Start () {
+		Instantiate (prefab, new Vector3 (4, 5, 0), new Quaternion ());
+		CreateMissile (SpawnPosition.TopRight, Direction.DownLeft);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateMissile(SpawnPosition pos, Direction dir) {
+		Instantiate (prefab, new Vector3 (4, -5, 0), DirectionToQuaternion(dir));
+	}
+
+	Quaternion DirectionToQuaternion(Direction dir) {
+		switch (dir) {
+		case Direction.DownLeft:
+			//return Quaternion.LookRotation(new Vector3(-0.5f, -0.5f, 0.0f));
+			return Quaternion.LookRotation(new Vector3(1.0f, 1.0f, 0.0f));
+		default:
+			Debug.Log("Not implemeted yet!");
+			return new Quaternion();
+		}
+	}
+
+	Vector3 SpawnPositionToPosition(SpawnPosition spawnPosition) {
+		return new Vector3(0.0f, -2.0f, 0.0f);
+	}
+}
