de9fceee6d0a6cea1819b97cebc1556fea7fed1a
woozie164 <woozie663@gmail.com>
2015-06-13 21:02:16 +0200

    Started on Wave1 scene

---
+++ Assets/AddThisToLevelScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddThisToLevelScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// This is needed to properly destroy the object when the level is destroyed.
+		GameObject levelManager = GameObject.Find ("LevelManager");
+		this.transform.parent = levelManager.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
