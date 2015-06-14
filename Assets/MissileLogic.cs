4cfb08672a5839719a74cdd7972e0df9eab22961
Jesper Tingvall <jesper@tingvall.pw>
2015-06-14 07:54:15 +0200

    Added new missile behaviour

--- Assets/MissileLogic.cs
+++ Assets/MissileLogic.cs
@@ -9,6 +9,17 @@ public class MissileLogic : MonoBehaviour {
 	public bool Homing = true;
 
 	private GameManagerScript manager;
+
+	public int explosionRadius = 20;
+	public int drillRadius = 10;
+	public int drillLength = 3;
+	public bool drill = false;
+	public bool clusterBomb = false;
+	public bool clusterBombChild = false;
+	public int clusterBombCount = 4;
+	private float clusterTimer = 1;
+
+	public MissileLogic ClusterChild;
 	
 	// Use this for initialization
 	void Start () {
@@ -29,12 +40,41 @@ public class MissileLogic : MonoBehaviour {
 			GetComponent<Rigidbody2D> ().velocity = speed * transform.forward;
 			if (cm) {
 				Vector2 pixel = cm.getTexturePosition (transform.position);
+
 				if (pixel.x != 0 || pixel.y != 0) {
-					if (cm.getCollision (pixel)) {
-						GetComponent<EnemyScript>().Explode();
+					if (cm.getCollision(pixel)) {
+						if (drill)
+							drillLength = drillOnCollision(pixel,drillLength);
+						else
+							dieOnCollision(pixel);
 					}
+				}
+				if (clusterBombChild) {
+					if (clusterTimer < 0)
+						dieOnCollision(pixel);
+					clusterTimer -= Time.deltaTime;
 				}
 			}
 		}
+	}
+
+	void dieOnCollision(Vector2 pixel) {
+		cm.removePixel(pixel,explosionRadius);
+		if (clusterBomb) {
+			while (clusterBombCount > 0) {
+				clusterBombCount--;
+				Quaternion rotation = Quaternion.LookRotation(new Vector3(Random.Range(-100,100),Random.Range(-100,100),0),new Vector3(0,0,1));
+				MissileLogic bullet = Instantiate(ClusterChild,transform.position,rotation) as MissileLogic;
+			}
+		}
+		GetComponent<EnemyScript>().Explode();
+	}
+	
+	int drillOnCollision(Vector2 pixel, int count) {
+		if (count > 0)
+			cm.removePixel(pixel,drillRadius);
+		else
+			dieOnCollision(pixel);
+		return count-1;
 	}
 }
